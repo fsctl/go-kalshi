bb96a07204bcc503d2956241ab333a20bf1bbe52
Mike Goelzer <mike@goelzer.org>
2022-04-28 13:05:57 -0600

    Add error checking

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+# Make sure '.env' file exists
+if [[ ! -f ".env" ]]; then
+	echo "Error: missing '.env' file that sets KALSHI_USERNAME and KALSHI_PASSWORD"
+	exit 1
+fi
+
+# Expose username/password in the environment
 source .env
 export KALSHI_USERNAME
 export KALSHI_PASSWORD
 
+# Make sure username/password are non-blank
+if [[ -z "$KALSHI_USERNAME" ]] || [[ -z "$KALSHI_PASSWORD" ]]; then
+	echo "Error: .env file needs to set KALSHI_USERNAME and KALSHI_PASSWORD variables"
+	exit 1
+fi
+
+# Run test binary
 ./list-markets
