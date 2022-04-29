69314c0af503437097a0c3b0c44693fa85da2862
Mike Goelzer <mike@goelzer.org>
2022-04-28 20:43:03 -0600

    read .env directly in go

--- cmd/kalshi-tool/main.go
+++ cmd/kalshi-tool/main.go
@@ -4,24 +4,36 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/fsctl/go-kalshi/pkg/kalshi"
+	"github.com/spf13/viper"
 )
+
+func readEnvFile() (string, string) {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error in readEnvFile: '%v'\n", err)
+	}
+	kalshiUsername, ok := viper.Get("KALSHI_USERNAME").(string)
+	if !ok {
+		log.Fatalf("Error: KALSHI_USERNAME was not a string")
+	}
+	kalshiPassword, ok := viper.Get("KALSHI_PASSWORD").(string)
+	if !ok {
+		log.Fatalf("Error: KALSHI_PASSWORD was not a string")
+	}
+	return kalshiUsername, kalshiPassword
+}
 
 func main() {
 	ctx := context.Background()
 
 	fmt.Printf("Kalshi Tool\n")
 
-	username := os.Getenv("KALSHI_USERNAME")
-	password := os.Getenv("KALSHI_PASSWORD")
-	if username == "" || password == "" {
-		log.Fatalf("Error:  username/password environment variables not set\n")
-		return
-	}
+	kalshiUsername, kalshiPassword := readEnvFile()
 
-	kc, err := kalshi.NewKalshiClient(ctx, username, password)
+	kc, err := kalshi.NewKalshiClient(ctx, kalshiUsername, kalshiPassword)
 	if err != nil {
 		log.Fatalf("Error:  NewKalshiClient failed: '%v'\n", err)
 		return
